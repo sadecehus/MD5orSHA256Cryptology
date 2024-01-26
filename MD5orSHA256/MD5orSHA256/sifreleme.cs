fe95bb3fe5e084c3847672426a2c82b8f8a7f992
sadecehus <89530597+sadecehus@users.noreply.github.com>
2024-01-26 12:29:41 +0300

    updated

---
+++ MD5orSHA256/MD5orSHA256/sifreleme.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example
+{
+    internal class sifreleme
+    {
+        public static string md5sifreleme(string sifrelenecekmetin)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            var arrayBytes = Encoding.UTF8.GetBytes(sifrelenecekmetin);
+            arrayBytes = md5.ComputeHash(arrayBytes);
+            var sb = new StringBuilder();
+            foreach (var item in arrayBytes)
+                sb.Append(item.ToString("X2").ToLower());
+            return sb.ToString();
+        }
+
+        public static string shaSifreleme(string sifrelenecekmetin)
+        {
+            SHA256 sha256 = new SHA256CryptoServiceProvider();
+            var arrayBytes = Encoding.UTF8.GetBytes(sifrelenecekmetin);
+            arrayBytes = sha256.ComputeHash(arrayBytes);
+            var sb = new StringBuilder();
+            foreach (var item in arrayBytes)
+                sb.Append(item.ToString("X2").ToLower());
+            return sb.ToString();
+        }
+    }
+}
